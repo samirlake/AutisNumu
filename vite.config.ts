3ea570b75bd3d3dca241c7b9635975a30adf94cd
samirlake <74074104+samirlake@users.noreply.github.com>
2026-02-28 22:45:55 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
+  }
 });
